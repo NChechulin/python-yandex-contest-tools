b9446b9262cc4a794af34712235aefdccb42281a
Nikolay Chechulin <nchechulin@protonmail.com>
2021-11-26 15:25:38 +0300

    add a warning if student's dir does not exist

--- src/info_strings.py
+++ src/info_strings.py
@@ -5,3 +5,6 @@ ERR_NO_CONFIG_FILE = colored("Config file does not exist", "red")
 ERR_CONFIG_INVALID_TOML = colored("Config is not valid TOML", "red")
 ERR_CONFIG_MISSING_FIELD = colored("Config file is missing a required field", "red")
 ERR_SUBMISSION_DOES_NOT_EXIST = colored("The submission file does not exist", "red")
+WARN_NO_STUDENT_DIR = colored(
+    "The directory with submissions of the student does not exist.\nPlease check the spelling of the name:", "yellow"
+)
