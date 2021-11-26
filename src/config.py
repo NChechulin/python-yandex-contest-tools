0980d29bd78413fe6b4da035397db5ec9646ec9c
Nikolay Chechulin <nchechulin@protonmail.com>
2021-11-26 13:28:35 +0300

    add a config object and implement loading from file

---
+++ src/config.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from toml import load as load_toml_file
+from toml.decoder import TomlDecodeError
+from typing import List
+from enum import Enum
+import info_strings
+from pathlib import Path
+from task import Task
+
+
+class OutputFormat(Enum):
+    CSV = "csv"
+    XLSX = "xlsx"
+
+
+@dataclass(init=False)
+class Config:
+    submissions_dir: str
+    output_format: OutputFormat
+    students_names: List[str]
+    tasks: List[Task]
+
+    def __init__(self, config_path: Path) -> "Config":
+        if not config_path.exists():
+            print(info_strings.ERR_NO_CONFIG_FILE)
+            exit(1)
+
+        try:
+            # TODO: try to avoid manual assignment
+            data = load_toml_file(config_path)["config"]
+            self.submissions_dir = data["submissions_dir"]
+            self.output_format = OutputFormat(data["output_format"])
+            self.students_names = data["students_names"]
+
+            self.tasks = []
+            for name, params in data["tasks"].items():
+                required_tokens = params.get("required_tokens", [])
+                banned_tokens = params.get("banned_tokens", [])
+                self.tasks.append(Task(name, required_tokens, banned_tokens))
+        except TomlDecodeError as e:
+            print(info_strings.ERR_CONFIG_INVALID_TOML, e)
+        except KeyError as e:
+            print(info_strings.ERR_CONFIG_MISSING_FIELD, e)
+            exit(1)
+
+
+if __name__ == "__main__":
+    path = Path("../config.toml")
+    config = Config(path)
+    print(config)
--- src/info_strings.py
+++ src/info_strings.py
@@ -2,6 +2,6 @@ from termcolor import colored
 
 
 ERR_NO_CONFIG_FILE = colored("Config file does not exist", "red")
-ERR_CONFIG_INVALID_JSON = colored("Config is not valid TOML", "red")
+ERR_CONFIG_INVALID_TOML = colored("Config is not valid TOML", "red")
 ERR_CONFIG_MISSING_FIELD = colored("Config file is missing a required field", "red")
 ERR_SUBMISSION_DOES_NOT_EXIST = colored("The submission file does not exist", "red")
